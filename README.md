037ef6d0b94732c591e739b6fdb650bd500662a4
Shoichi Kaji <skaji@cpan.org>
2018-03-13 22:55:08 +0900

    init

--- README.md
+++ README.md
@@ -10,7 +10,21 @@ CPAN::Test::Dummy::Perl5::Simple - What is CPAN module?
 
 # DESCRIPTION
 
-CPAN::Test::Dummy::Perl5::Simple is
+What is CPAN module?
+
+I think that we do not need:
+
+- Makefile.PL
+
+    because we now have the concept of [Static Install](http://blogs.perl.org/users/shoichi_kaji1/2017/03/make-your-cpan-module-static-installable.html).
+
+- META.yml
+
+    because we now have a new format, `META.json`.
+
+- MANIFEST
+
+    do we really need MANIFEST file? See [github.com/perl6/modules.perl6.org](https://github.com/perl6/modules.perl6.org/commit/f1af625ae4c2806d3be79821f75af28f6270d62c).
 
 # AUTHOR
 
--- lib/CPAN/Test/Dummy/Perl5/Simple.pm
+++ lib/CPAN/Test/Dummy/Perl5/Simple.pm
@@ -19,7 +19,25 @@ CPAN::Test::Dummy::Perl5::Simple - What is CPAN module?
 
 =head1 DESCRIPTION
 
-CPAN::Test::Dummy::Perl5::Simple is
+What is CPAN module?
+
+I think that we do not need:
+
+=over 4
+
+=item Makefile.PL
+
+because we now have the concept of L<Static Install|http://blogs.perl.org/users/shoichi_kaji1/2017/03/make-your-cpan-module-static-installable.html>.
+
+=item META.yml
+
+because we now have a new format, C<META.json>.
+
+=item MANIFEST
+
+do we really need MANIFEST file? See L<github.com/perl6/modules.perl6.org|https://github.com/perl6/modules.perl6.org/commit/f1af625ae4c2806d3be79821f75af28f6270d62c>.
+
+=back
 
 =head1 AUTHOR
 
